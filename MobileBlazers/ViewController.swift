0832271923789769b1c0c9e82a527022c08f5eb2
Devendranath <dnreddy890@gmail.com>
2019-09-19 07:39:08 +0530

    Initial Commit

---
+++ MobileBlazers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileBlazers
+//
+//  Created by apple on 19/09/19.
+//  Copyright © 2019 iOSProofs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
