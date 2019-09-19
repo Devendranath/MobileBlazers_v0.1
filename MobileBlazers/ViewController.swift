6c0a286850467f36967b1632f8e85bc831e45735
Devendranath <dnreddy890@gmail.com>
2019-09-19 07:43:04 +0530

    Implemented UploadImages Features.

--- MobileBlazers/ViewController.swift
+++ MobileBlazers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let uiVC = UploadImagesViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
